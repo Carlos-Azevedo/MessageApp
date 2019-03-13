1340c383944c23739af4aa35c1f4e2bc8da0cc93
Carlos.Azevedo <carlosfa3@gmail.com>
2019-03-13 22:13:26 +0000

    Added User Contructor

--- MessageApp/MessageAppModels/User.cs
+++ MessageApp/MessageAppModels/User.cs
@@ -9,5 +9,12 @@ namespace MessageAppModels
         public string Name { get; set; }
         public List<string> Following { get; set; }
         public List<Message> Messages { get; set; }
+
+        public User(string user)
+        {
+            Name = user;
+            Following = new List<string>();
+            Messages = new List<Message>();
+        }
     }
 }
