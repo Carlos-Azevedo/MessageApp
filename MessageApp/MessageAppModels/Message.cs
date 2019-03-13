815a3d329c790f5b21be869b72b2695084cb7d30
Carlos.Azevedo <carlosfa3@gmail.com>
2019-03-13 21:25:52 +0000

    Added Models For Project

---
+++ MessageApp/MessageAppModels/Message.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace MessageAppModels
+{
+    public class Message
+    {
+        public string Contents { get; set; }
+        public DateTime PostTIme { get; set; }
+    }
+}
---
+++ MessageApp/MessageAppModels/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MessageAppModels
+{
+    public class User
+    {
+        public string Name { get; set; }
+        public List<string> Following { get; set; }
+        public List<Message> Messages { get; set; }
+    }
+}
